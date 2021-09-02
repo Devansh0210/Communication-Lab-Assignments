7350db6f19ef7195780574d39c23955746e2baa5
Devansh0210 <devansht0210@gmail.com>
2021-09-02 13:46:47 +0530

    Changed Lab-2 Task-2

--- Lab2/Task2.py
+++ Lab2/Task2.py
@@ -1,8 +1,7 @@
-import numpy as np
+import math
 import matplotlib.pyplot as plt
 
-
-# Power received
+# Function for Power received
 def Pr(f, d, Pt=1, Gt=10, Gr=0):
     """
 
@@ -13,21 +12,35 @@ def Pr(f, d, Pt=1, Gt=10, Gr=0):
     :param float Gr: Antenna Gain of Receiver (dB)
     :return: Power Received in dBm
     """
-    Gt = np.exp(Gt / 10)
-    Gr = np.exp(Gr / 10)
+    Gt = 10**(Gt / 10)
+    Gr = 10**(Gr / 10)
     l = 3.0e8 / f
-    P = Pt * Gt * Gr * np.square(l) / np.square(4 * np.pi * d)
-    return 10.0 * np.log10(P) + 30  # Power in dBm = 10log10(P/1mW)
+    P = Pt * Gt * Gr * l**2 / (4 * math.pi * d)**2
+    return 10.0 * math.log10(P) + 30 # Power in dBm = 10log10(P/1mW)
 
 
+# First freq
 f1 = 900e6
+
+# Second Freq
 f2 = 2.4e9
 
-d = np.arange(100, 5000, 500)
+# distance
+d = []
 
-Pr_f1 = Pr(f1, d)
-Pr_f2 = Pr(f2, d)
+# Power received for freq f1
+Pr_f1 = []
 
+# Power received for freq f2
+Pr_f2 = []
+
+# Loop for calculating power at different distance from 100m to 5000m with step of 500m
+for i in range(100, 5001, 500):
+    d.append(i)
+    Pr_f1.append(Pr(f1, i))
+    Pr_f2.append(Pr(f2, i))
+
+# Plotting the Power vs Distance
 fig, ax = plt.subplots()
 ax.plot(d, Pr_f1)
 ax.plot(d, Pr_f2)
